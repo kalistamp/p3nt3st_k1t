5fb9561f4ee1f0c8e0913dffa058f6dff5c729dd
kalistamp <81341243+kalistamp@users.noreply.github.com>
2022-04-09 15:13:50 -0700

    Create toolkit.py

---
+++ toolkit.py
@@ -0,0 +1,15 @@
+
+# Note: If ran as sudo Permissions will default to "Locked"
+
+file.open()
+list.write("""
+
+Resources:
+
+Short guide to get started:
+
+https://github.com/DennisFeldbusch/CheatSheet
+
+
+""")
+file.close()
